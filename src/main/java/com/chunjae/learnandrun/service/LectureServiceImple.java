898ae617b333b7efc3906e5de4a50ce0633cfa92
star <ebyeol628@gmail.com>
2024-05-22 01:23:44 +0900

    0521 강의 crud

--- src/main/java/com/chunjae/learnandrun/service/LectureServiceImple.java
+++ src/main/java/com/chunjae/learnandrun/service/LectureServiceImple.java
@@ -2,11 +2,18 @@ package com.chunjae.learnandrun.service;
 
 import com.chunjae.learnandrun.dao.LectureMapper;
 import com.chunjae.learnandrun.dto.LectureDTO;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class LectureServiceImple implements LectureService{
@@ -51,5 +58,118 @@ public class LectureServiceImple implements LectureService{
     public int deleteLecture(int lectureNo) {
         int result=mapper.deleteLecture(lectureNo);
         return result;
+    }
+
+    @Override
+    public int insertLecture(String uploadPath, LectureDTO dto) {
+
+        String thumbnail=thumbnailUpload(uploadPath, dto);
+        dto.setThumbnail(thumbnail);
+
+        String zip=zipUpload(uploadPath, dto);
+        dto.setLectureData(zip);
+
+        int result=mapper.insertLecture(dto);
+
+//        int result=mapper.insertLecture();
+
+
+        return result;
+    }
+
+    @Override
+    public void updateLecture(String uploadPath, LectureDTO dto) {
+
+        String thumbnail=thumbnailUpload(uploadPath, dto);
+        dto.setThumbnail(thumbnail);
+        
+        //수정 전 파일 삭제 기능 추가하기
+
+        String zip=zipUpload(uploadPath, dto);
+        dto.setLectureData(zip);
+        
+        mapper.updateLecture(dto);
+    }
+
+    /** 썸네일 업로드 */
+    private String thumbnailUpload(String uploadPath, LectureDTO dto){
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println("가나다라");
+        System.out.println();
+        System.out.println();
+        MultipartFile multi= dto.getThumbnailFile();
+
+
+        String originalFilename=multi.getOriginalFilename();
+        originalFilename= URLEncoder.encode(originalFilename, StandardCharsets.UTF_8)
+                .replace("+", "%20");
+        String thumbnailName="";
+
+        try{
+            if(!multi.isEmpty()){
+                UUID uuid= UUID.randomUUID();
+                String fileName=uuid+"_"+originalFilename;
+                File file=new File(uploadPath, fileName);
+                multi.transferTo(file);
+
+
+                //썸네일 업로드
+                thumbnailName="thumbnail_"+uuid+"_"+originalFilename;
+                String thumbnailPath=uploadPath+"/"+thumbnailName;
+
+                File thumbnail=new File(thumbnailPath);
+                Thumbnailator.createThumbnail(file, thumbnail, 240, 180);
+
+                //로컬에 업로드
+                String localPath="D:\\LearnAndRun_test\\src\\main\\webapp\\upload\\img"
+                        +"/"+"thumbnail_"+uuid+"_"+originalFilename;
+                File thumbnailLocal=new File(localPath);
+                Thumbnailator.createThumbnail(file, thumbnailLocal, 240, 180);
+
+                //원본 이미지 삭제
+                file.delete();
+
+            }
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
+        return thumbnailName;
+    }
+
+    /** zip파일 업로드 */
+    private String zipUpload(String uploadPath, LectureDTO dto){
+
+        MultipartFile multi= dto.getLectureDataFile();
+
+
+        String originalFilename=multi.getOriginalFilename();
+        originalFilename= URLEncoder.encode(originalFilename, StandardCharsets.UTF_8)
+                .replace("+", "%20");
+        String zipName="";
+
+        try{
+            if(!multi.isEmpty()){
+                UUID uuid= UUID.randomUUID();
+                String fileName=uuid+"_"+originalFilename;
+                File file=new File(uploadPath, fileName);
+                multi.transferTo(file);
+
+                zipName="lecture_"+uuid+"_"+originalFilename;
+
+                //로컬에 업로드
+                String localPath="D:\\LearnAndRun_test\\src\\main\\webapp\\upload\\img"
+                        +"/"+"lecture_"+uuid+"_"+originalFilename;
+                File zipLocal=new File(localPath);
+                multi.transferTo(zipLocal);
+            }
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
+        return zipName;
     }
 }
--- src/main/webapp/WEB-INF/views/lecture/lecture_update.jsp
+++ src/main/webapp/WEB-INF/views/lecture/lecture_update.jsp
@@ -2,33 +2,33 @@
 <html>
 <head>
     <title>Title</title>
-    <link rel="stylesheet" href="/resources/css/lecture_insert.css">
 </head>
 <body>
 <div id="wrap">
-    <h2>강의 등록</h2>
+    <h2>강의 수정</h2>
     <div class="line"></div>
-    <form method="post" action="/lecture_insert_result">
+    <form method="post" action="/lecture_update_result" enctype="multipart/form-data">
         <ul>
             <li>
-                <label for="thumbnail">썸네일</label>
-                <input type="file" name="thumbnail" id="thumbnail">
+                <label for="thumbnailFile">썸네일</label>
+                <img src="/getImage/${dto.thumbnail}" alt="${dto.thumbnail}">
+                <input type="file" name="thumbnailFile" id="thumbnailFile" value="${dto.thumbnail}">
             </li>
             <li>
                 <label for="lectureName">강의명</label>
-                <input type="text" name="lectureName" id="lectureName">
+                <input type="text" name="lectureName" id="lectureName" value="${dto.lectureName}">
             </li>
             <li>
                 <label for="teacher">강사명</label>
-                <input type="text" name="teacher" id="teacher">
+                <input type="text" name="teacher" id="teacher" value="${dto.teacher}">
             </li>
             <li>
                 <label for="description">강의설명</label>
-                <textarea cols="40" rows="5" maxlength="250" name="description" id="description"></textarea>
+                <textarea cols="40" rows="5" maxlength="250" name="description" id="description">${dto.description}</textarea>
             </li>
             <li>
                 <label for="subjectName">과목</label>
-                <select name="subjectName" id="subjectName">
+                <select name="subjectName" id="subjectName" >
                     <option value="국어">국어</option>
                     <option value="수학">수학</option>
                     <option value="영어">영어</option>
@@ -46,17 +46,18 @@
             </li>
             <li>
                 <label for="price">가격</label>
-                <input type="text" name="price" id="price">
+                <input type="text" name="price" id="price" value="${dto.price}">
             </li>
             <li>
                 <label for="startDate">개강일</label>
-                <input type="date" name="startDate" id="startDate">
+                <input type="date" name="startDate" id="startDate" value="${dto.startDate}">
             </li>
             <li>
-                <label for="lectureData">강의자료</label>
-                <input type="file" name="lectureData" id="lectureData">
+                <label for="lectureDataFile">강의자료</label>
+                <input type="file" name="lectureDataFile" id="lectureDataFile" datasrc="/getImage/${dto.thumbnail}">
             </li>
             <li>
+                <input type="hidden" name="lectureNo" value="${dto.lectureNo}">
                 <input type="submit" value="등록하기">
             </li>
         </ul>
