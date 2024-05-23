f0af9f70a0bd8a14444a9872194467f00a75c205
HyunJun <skd97122@gmail.com>
2024-05-23 17:45:35 +0900

    css + 관리자페이지 작업

--- src/main/java/com/chunjae/learnandrun/service/OrderServiceImple.java
+++ src/main/java/com/chunjae/learnandrun/service/OrderServiceImple.java
@@ -1,11 +1,14 @@
 package com.chunjae.learnandrun.service;
 
 import com.chunjae.learnandrun.dao.OrderMapper;
+import com.chunjae.learnandrun.dto.OrderDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service("orderService")
 @RequiredArgsConstructor
@@ -22,5 +25,17 @@ public class OrderServiceImple implements OrderService{
         hm.put("order_id", order_id);
 
         orderMapper.insertOrder(hm);
+    }
+
+    @Override
+    public List<OrderDTO> listOrder() {
+        return orderMapper.listOrder();
+    }
+
+    @Override
+    public void updateAuthorities(Map<String, Boolean> hm) {
+        for(Map.Entry<String,Boolean> entry: hm.entrySet()){
+            orderMapper.updateAuthorities(entry.getKey(),entry.getValue());
+        }
     }
 }
